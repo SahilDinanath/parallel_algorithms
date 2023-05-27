a325d039600eb272cdb05b26598cf6f05acb7300
GyuoMi <darshnsingh10@gmail.com>
2023-05-27 13:54:18 +0200

    added bitonicsort check out the main methods for each implementation, it has my reading in from txt

---
+++ bitonicsort/bitonic.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void compAndSwap(int* a, int i, int j, int dir) {
+    if ((a[i] > a[j] && dir == 1) || (a[i] < a[j] && dir == 0)) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
+
+void bitonicMerge(int* a, int low, int count, int dir) {
+    if (count > 1) {
+        int k = count / 2;
+        for (int i = low; i < low + k; i++)
+            compAndSwap(a, i, i + k, dir);
+        
+        bitonicMerge(a, low, k, dir);
+        bitonicMerge(a, low + k, k, dir);
+    }
+}
+
+void bitonicSort(int* a, int low, int count, int dir) {
+    if (count > 1) {
+        int k = count / 2;
+        
+        bitonicSort(a, low, k, 1);
+        bitonicSort(a, low + k, k, 0);
+        
+        bitonicMerge(a, low, count, dir);
+    }
+}
+
+int main() {
+   FILE* file = fopen("arrays.txt", "r");
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    int a[MAX_SIZE];
+    int count = 0;
+    int num;
+
+    while (fscanf(file, "%d", &num) == 1) {
+        a[count] = num;
+        count++;
+    }
+
+    fclose(file);
+
+    printf("Original array: ");
+    for (int i = 0; i < count; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    bitonicSort(a, 0, count, 1);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < count; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    return 0; 
+    
+    return 0;
+}
