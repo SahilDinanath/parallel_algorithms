9914a19a1ec2fa4c72e4296d2f28c23459cb5a40
Sahil Dinanath <sahildinanath@gmail.com>
2023-05-27 14:40:24 +0200

    input files can now be read

--- scan/scan_loop_carried.c
+++ scan/scan_loop_carried.c
@@ -1,54 +1,44 @@
-#include <stdio.h>
-#include <omp.h>
-#include <time.h>
-#include <stdlib.h>
 #include <math.h>
-void generateArrayOfRandomNumbers(int input[],int size){
-  srand(time(NULL));
-  for(int i = 0; i < size; i++){
-    input[i] = rand()%100;
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE pow(2, 16)
+
+void printArray(int input[], int startIndex, int endIndex) {
+  for (int i = startIndex; i < endIndex; i++) {
+    printf("%d ", input[i]);
   }
 }
-void printArray(int input[],int startIndex,int endIndex){
-  for(int i = startIndex; i < endIndex;i++){
-    printf("%d ",input[i]);
+void readFile(char *line, int size, char *fileName) {
+  FILE *file = fopen(fileName, "r");
+
+  fgets(line, size, file);
+  fclose(file);
+}
+void convertCharToIntArray(char *fileCharacters, int *input, int size) {
+  for (int i = 0; i < size; i++) {
+    input[i] = fileCharacters[i] - '0';
   }
 }
 
-int main(int argc, char *argv[])
-{
-
-  while(fgets(line,100,p_file) != NULL)
-{
-    char *p = strtok(line, ":");
-    while(p)
-    {
-        printf("%s\n", p); //Store into array or variables
-        p=strtok(NULL, ":");
-    }
+void prefixSum(int *input, int size) {
+  for (int i = 1; i < size; i++) {
+    input[i] += input[i - 1];
+  }
 }
-  int *input = malloc(inputSize*sizeof(int));
+int main(int argc, char *argv[]) {
+  //input size of file is one larger then the actual input as the last value in the array of reading in any string is '\0'  
+  int inputSize = (int)SIZE;
+  int inputSizeOfTextFile = inputSize+1;
 
-  for(int i = 1; i < argc; i++){
-    input[i-1] = atoi(argv[i]);
-  }
-  // int input[] = {3,1,7,0,4,1,6,3};
-  // int input[131072*8];
-  // int inputSize = sizeof(input)/sizeof(int);
-  // generateArrayOfRandomNumbers(input, inputSize);
+  char fileInput[inputSizeOfTextFile];
+  int input[inputSize];
 
-  
-  double timer = omp_get_wtime();
-  
-  for(int i = 1; i < inputSize; i++){
-    input[i] += input[i-1];
-  }
-  
+  readFile(fileInput, inputSizeOfTextFile, "input_2_16.txt");
 
-  timer = omp_get_wtime()-timer;
+  convertCharToIntArray(fileInput, input, inputSize);
+
+  prefixSum(input, inputSize);
+
   printArray(input, 0, inputSize);
-  printf("\n");
-  printf("time taken: %f\n",timer);
-  free(input);
   return 0;
 }
