0902f158e6e289dce91d289395546b9619e0b7ec
Sahil Dinanath <sahildinanath@gmail.com>
2023-05-27 18:43:24 +0200

    now supports reading in file, size of problem and threads at run time

--- scan/scan_omp.c
+++ scan/scan_omp.c
@@ -2,190 +2,87 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#define THREADS 8
 
-void getUpSweep(int input[], int size) {
-
-  int treeDepth = ceil(log2(size));
-  int increment;
-  for (int i = 0; i < treeDepth; i++) {
-    increment = pow(2, i + 1);
-#pragma omp parallel
-    {
-      int previous, next;
-#pragma omp for schedule(static, size / THREADS)
-      for (int j = 0; j < size; j += increment) {
-        previous = j + pow(2, i) - 1;
-        next = j + pow(2, i + 1) - 1;
-        input[next] = input[previous] + input[next];
-      }
-    }
+void printArray(long input[], long startIndex, long endIndex) {
+  for (long i = startIndex; i < endIndex; i++) {
+    printf("%ld ", input[i]);
   }
 }
-void getUpSweepUsingProcessorSum(int input[], int size) {
+void readFile(char *line, long size, char *fileName) {
+  FILE *file = fopen(fileName, "r");
 
-  int chunkSize = size / THREADS;
-  int treeDepth = ceil(log2(chunkSize));
-#pragma omp parallel
-  {
-    int processor = omp_get_thread_num();
-    int previous, next;
-    int increment;
-    for (int i = 0; i < treeDepth; i++) {
-      increment = pow(2, i + 1);
-      for (int j = chunkSize * processor; j < chunkSize * (processor + 1);
-           j += increment) {
-        previous = j + pow(2, i) - 1;
-        next = j + pow(2, i + 1) - 1;
-        input[next] = input[previous] + input[next];
-      }
+  fgets(line, size, file);
+  fclose(file);
+}
+void convertCharToIntArray(char *fileCharacters, long *input, long size) {
+  for (long i = 0; i < size; i++) {
+    input[i] = fileCharacters[i] - '0';
+  }
+}
+
+void upSweep(long input[], long size) {
+  long previous, next;
+  long treeDepth = ceil(log2(size));
+  for (long i = 0; i < treeDepth; i++) {
+    for (long j = 0; j < size; j += pow(2, i + 1)) {
+      previous = j + pow(2, i) - 1;
+      next = j + pow(2, i + 1) - 1;
+      input[next] = input[previous] + input[next];
     }
   }
 }
 
-void getDownSweep(int input[], int size) {
-  int treeDepth = ceil(log2(size));
+void downSweep(long input[], long size) {
+  long previous, next, temp;
   input[size - 1] = 0;
-
-  for (int i = treeDepth - 1; i >= 0; i--) {
-#pragma omp parallel
-    {
-      int previous, next, temp;
-      int increment = pow(2, i + 1);
-#pragma omp for schedule(static, size / THREADS)
-      for (int j = 0; j < size; j += increment) {
-        previous = j + pow(2, i) - 1;
-        next = j + pow(2, i + 1) - 1;
-        temp = input[previous];
-        input[previous] = input[next];
-        input[next] = temp + input[next];
-      }
-    }
-  }
-}
-void generateArrayOfRandomNumbers(int input[], int size) {
-  srand(time(NULL));
-  for (int i = 0; i < size; i++) {
-    input[i] = rand() % 100;
-  }
-}
-void getDownSweepUsingProcessorSum(int input[], int size) {
-  input[size - 1] = 0;
-  int chunkSize = size / THREADS;
-  int treeDepth = ceil(log2(chunkSize));
-#pragma omp parallel
-  {
-    int processor = omp_get_thread_num();
-
-    for (int i = treeDepth - 1; i >= 0; i--) {
-      int previous, next, temp;
-      int increment = pow(2, i + 1);
-      for (int j = processor * chunkSize; j < chunkSize * (processor + 1);
-           j += increment) {
-        previous = j + pow(2, i) - 1;
-        next = j + pow(2, i + 1) - 1;
-        temp = input[previous];
-        input[previous] = input[next];
-        input[next] = temp + input[next];
-      }
+  long treeDepth = ceil(log2(size));
+  for (long i = treeDepth - 1; i >= 0; i--) {
+    for (long j = 0; j < size; j += pow(2, i + 1)) {
+      previous = j + pow(2, i) - 1;
+      next = j + pow(2, i + 1) - 1;
+      temp = input[previous];
+      input[previous] = input[next];
+      input[next] = temp + input[next];
     }
   }
 }
 
-void printArray(int input[], int startIndex, int endIndex) {
-  for (int i = startIndex; i < endIndex; i++) {
-    printf("%d ", input[i]);
-  }
+void prefixSum(long *input, long size) {
+  upSweep(input, size);
+  downSweep(input, size);
 }
 
-void getProcessorSum(int input[], int processorSum[], int size) {
-#pragma omp parallel
-  {
-    int processor = omp_get_thread_num();
-    int numOfProcessors = omp_get_num_threads();
-
-    int chunkSize = size / numOfProcessors;
-
-    processorSum[processor] = input[chunkSize * processor];
-
-    for (int i = 1; i < chunkSize; i++) {
-      processorSum[processor] =
-          processorSum[processor] + input[chunkSize * processor + i];
-    }
-  }
-  getUpSweep(processorSum, THREADS);
-  getDownSweep(processorSum, THREADS);
-}
-
-void addProcessorSumsToPrescan(int input[], int size, int processorSum[]) {
-
-  int chunkSize = size / THREADS;
-#pragma omp parallel
-  {
-    int processor = omp_get_thread_num();
-    for (int j = chunkSize * processor; j < chunkSize * (processor + 1); j++) {
-      input[j] = processorSum[processor] + input[j];
-    }
-  }
-}
-double averageOfRuns(int input[], int size, int runs) {
-  double average = 0;
-  for (int i = 0; i < runs; i++) {
-    double timer = omp_get_wtime();
-    getUpSweep(input, size);
-    getDownSweep(input, size);
-    timer = omp_get_wtime() - timer;
-    average += timer;
-  }
-  return average / runs;
-}
 int main(int argc, char *argv[]) {
+  char *inputFile = argv[1];
+  long inputSize = atoll(argv[2]);
+  int threads = atoi(argv[3]);
+
   omp_set_dynamic(0);
-  omp_set_num_threads(THREADS);
-  int processorSum[THREADS];
+  omp_set_num_threads(threads);
 
-  FILE *file = fopen("input_2_16.txt", "r");
-  int *input;
-  size_t n = 0;
-  int c;
+  long inputSizeOfTextFile = inputSize+1;
+  char *fileInput = (char*)malloc(inputSize*sizeof(char));
+  long *input = (long*)malloc(inputSize*sizeof(long));
 
-  fseek(file, 0, SEEK_END);
-  long f_size = ftell(file);
-  fseek(file, 0, SEEK_SET);
-  input = (int *)malloc(f_size * sizeof(int));
+  readFile(fileInput, inputSizeOfTextFile, inputFile);
+  convertCharToIntArray(fileInput, input, inputSize);
 
-  while ((c = fgetc(file)) != EOF) {
-    input[n++] = (int)c;
-  }
+  //start timing code here
+  double timeStart = omp_get_wtime();
 
-  int inputSize = f_size;
-  // int input[] = {3, 1, 7, 0, 4, 1, 6, 3};
-  // int input[131072 * 8];
-  // int inputSize = sizeof(input) / sizeof(int);
+  //put algorithm here
+  prefixSum(input, inputSize);
 
-  // generateArrayOfRandomNumbers(input, inputSize);
-  int lastNum = input[inputSize - 1];
+  //stop timing code here
+  double timeStop = omp_get_wtime();
+  double timeTaken = timeStop - timeStart;
 
-  double timer = averageOfRuns(input, inputSize, 10);
 
-  // getProcessorSum(input, processorSum, inputSize);
-  // printArray(processorSum, 0, THREADS);
-  // printf("\n");
-  // getUpSweep(input, inputSize);
-  // getDownSweep(input , inputSize);
-  // getUpSweepUsingProcessorSum(input, inputSize);
-  // printf("array after adding processr sums to up sweep:\n");
+  //print out time taken
+  printf("%f",timeTaken);
   // printArray(input, 0, inputSize);
-  // printf("\n");
 
-  // getDownSweepUsingProcessorSum(input, inputSize);
-  // addProcessorSumsToPrescan(input, inputSize, processorSum);
+  printf("\n");
 
-  // printArray(input, 1, inputSize);
-  // printf("%d",input[inputSize-1]+lastNum);
-  // printf("\n");
-
-  printf("time taken: %f\n", timer);
   return 0;
 }
