126d89c42271579b3034aef641b2f1f118d4095f
Sahil Dinanath <sahildinanath@gmail.com>
2023-05-27 15:54:29 +0200

    changed ints to long to prevent buffer overflow

--- scan/scan_loop_carried.c
+++ scan/scan_loop_carried.c
@@ -1,44 +1,57 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
+//power should correspond to the text file
 #define SIZE pow(2, 16)
 
-void printArray(int input[], int startIndex, int endIndex) {
-  for (int i = startIndex; i < endIndex; i++) {
-    printf("%d ", input[i]);
+void printArray(long input[], long startIndex, long endIndex) {
+  for (long i = startIndex; i < endIndex; i++) {
+    printf("%ld ", input[i]);
   }
 }
-void readFile(char *line, int size, char *fileName) {
+void readFile(char *line, long size, char *fileName) {
   FILE *file = fopen(fileName, "r");
 
   fgets(line, size, file);
   fclose(file);
 }
-void convertCharToIntArray(char *fileCharacters, int *input, int size) {
-  for (int i = 0; i < size; i++) {
+void convertCharToIntArray(char *fileCharacters, long *input, long size) {
+  for (long i = 0; i < size; i++) {
     input[i] = fileCharacters[i] - '0';
   }
 }
 
-void prefixSum(int *input, int size) {
-  for (int i = 1; i < size; i++) {
+void prefixSum(long *input, long size) {
+  for (long i = 1; i < size; i++) {
     input[i] += input[i - 1];
   }
 }
 int main(int argc, char *argv[]) {
   //input size of file is one larger then the actual input as the last value in the array of reading in any string is '\0'  
-  int inputSize = (int)SIZE;
-  int inputSizeOfTextFile = inputSize+1;
 
+  char *inputFile = argv[1];
+  long inputSize = (long)SIZE;
+
+  long inputSizeOfTextFile = inputSize+1;
   char fileInput[inputSizeOfTextFile];
-  int input[inputSize];
+  long input[inputSize];
 
-  readFile(fileInput, inputSizeOfTextFile, "input_2_16.txt");
-
+  readFile(fileInput, inputSizeOfTextFile, inputFile);
   convertCharToIntArray(fileInput, input, inputSize);
 
+  //start timing code here
+  double timeStart = omp_get_wtime();
+
+  //put algorithm here
   prefixSum(input, inputSize);
 
+  //stop timing code here
+  double timeStop = omp_get_wtime();
+  double timeTaken = timeStop - timeStart;
+  // printf("%f",timeTaken);
   printArray(input, 0, inputSize);
+
+  printf("\n");
   return 0;
 }
