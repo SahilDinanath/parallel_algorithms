d573439009903ddf96873f6a15c26c3a06e3d39c
Sahil Dinanath <sahildinanath@gmail.com>
2023-05-28 10:36:15 +0200

    implemented the changes in the scan_omp.c file

--- scan/scan_omp.c
+++ scan/scan_omp.c
@@ -8,18 +8,20 @@ void printArray(long input[], long startIndex, long endIndex) {
     printf("%ld ", input[i]);
   }
 }
-void readFile(char *line, long size, char *fileName) {
-  FILE *file = fopen(fileName, "r");
-
-  fgets(line, size, file);
-  fclose(file);
+void getFileSize(long *size,FILE *fileName){
+  fseek(fileName,0L,SEEK_END);
+  *size = ftell(fileName);
+  fseek(fileName,0L,SEEK_SET);
+}
+void readFile(char *line, long size, FILE *fileName) {
+  fgets(line, size, fileName);
+  fclose(fileName);
 }
 void convertCharToIntArray(char *fileCharacters, long *input, long size) {
   for (long i = 0; i < size; i++) {
     input[i] = fileCharacters[i] - '0';
   }
 }
-
 void upSweep(long input[], long size) {
   long previous, next;
   long treeDepth = ceil(log2(size));
@@ -72,24 +74,27 @@ void prefixSum(long *input, long size) {
 
 int main(int argc, char *argv[]) {
   char *inputFile = argv[1];
-  long inputSize = atoll(argv[2]);
-  int threads = atoi(argv[3]);
+  int threads = atoi(argv[2]);
+  long inputFileSize;
+  long inputArraySize;
+  FILE *file = fopen(inputFile, "r");
+  getFileSize(&inputFileSize, file);
+  inputArraySize = inputFileSize-1;
 
   omp_set_dynamic(0);
   omp_set_num_threads(threads);
 
-  long inputSizeOfTextFile = inputSize + 1;
-  char *fileInput = (char *)malloc(inputSize * sizeof(char));
-  long *input = (long *)malloc(inputSize * sizeof(long));
+  char *inputFromFile = (char*)malloc(inputArraySize*sizeof(char));
+  long *input = (long *)malloc(inputArraySize* sizeof(long));
 
-  readFile(fileInput, inputSizeOfTextFile, inputFile);
-  convertCharToIntArray(fileInput, input, inputSize);
+  readFile(inputFromFile, inputFileSize, file);
+  convertCharToIntArray(inputFromFile, input, inputArraySize);
 
   // start timing code here
   double timeStart = omp_get_wtime();
 
   // put algorithm here
-  prefixSum(input, inputSize);
+  prefixSum(input, inputArraySize);
 
   // stop timing code here
   double timeStop = omp_get_wtime();
@@ -97,7 +102,7 @@ int main(int argc, char *argv[]) {
 
   // print out time taken
   printf("%f", timeTaken);
-  // printArray(input, 0, inputSize);
+  // printArray(input, 0, inputArraySize);
 
   printf("\n");
 
