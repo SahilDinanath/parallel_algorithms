0f7f6783dfb62d6608d3a58b92e005513a8f9a5d
Sahil Dinanath <sahildinanath@gmail.com>
2023-05-28 09:40:20 +0200

    added some run configurations

--- scan/run.sh
+++ scan/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 num_of_runs=100
-num_of_threads = 10
 
 make clean
 make all 
@@ -11,10 +10,18 @@ echo "Results of Scan Implementations"
 echo "========================"
 echo "scan:"
 #the parameters that scan take are ./scan <file_name> <number_of_digits_of_file>
-./scan input_2_16.txt 65536
+# ./scan input_2_32.txt 4294967296
+./scan input_2_28.txt 268435456 
+# ./scan input_2_26.txt 67108864
+# ./scan input_2_24.txt 16777216
+# ./scan input_2_16.txt 65536
 echo ""
 echo "scan_omp:"
 #the parameters that scan_omp take are ./scan_omp <file_name> <number_of_digits_of_file> <threads>
-./scan_omp input_2_16.txt 65536 4
+./scan_omp input_2_28.txt 268435456 2
+# ./scan_omp input_2_26.txt 67108864 8
+# ./scan_omp input_2_36.txt 4294967296 4
+# ./scan_omp input_2_24.txt 16777216 4
+# ./scan_omp input_2_16.txt 65536 4
 echo ""
 echo "========================"
