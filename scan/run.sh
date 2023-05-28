a6feeff228e67f79e45a5eeadb618912b0e26bab
Sahil Dinanath <sahildinanath@gmail.com>
2023-05-28 12:18:34 +0200

    added mpi run to make file

--- scan/run.sh
+++ scan/run.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
-
-num_of_runs=100
+#options for running files
+file_name=input_2_28.txt
+threads=4
+processes=4
 
 make clean
 make all 
+
+
 
 echo "========================"
 echo "Results of Scan Implementations"
 echo "========================"
 echo "scan:"
-#the parameters that scan take are ./scan <file_name> <number_of_digits_of_file>
-# ./scan input_2_32.txt 4294967296
-./scan input_2_28.txt 268435456 
-# ./scan input_2_26.txt 67108864
-# ./scan input_2_24.txt 16777216
-# ./scan input_2_16.txt 65536
+#the parameters that scan take are ./scan <file_name> 
+./scan ${file_name}
 echo ""
 echo "scan_omp:"
-#the parameters that scan_omp take are ./scan_omp <file_name> <number_of_digits_of_file> <threads>
-./scan_omp input_2_28.txt 268435456 2
-# ./scan_omp input_2_26.txt 67108864 8
-# ./scan_omp input_2_36.txt 4294967296 4
-# ./scan_omp input_2_24.txt 16777216 4
-# ./scan_omp input_2_16.txt 65536 4
+#the parameters that scan_omp take are ./scan_omp <file_name> <threads>
+./scan_omp ${file_name} ${threads}
 echo ""
+echo "scan_mpi:"
+mpirun -np ${processes} ./scan_mpi ${file_name} 
 echo "========================"
