99e55fbf7210882926db70803a6ed65290160a50
Sahil Dinanath <sahildinanath@gmail.com>
2023-06-02 20:59:05 +0200

    modified run script to implement omp implementation

--- sssp_dijkstra/run.sh
+++ sssp_dijkstra/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 file_name="graph_10.txt"
+threads=2
+
 make clean
 make all
 cd input_graphs
@@ -11,4 +13,8 @@ echo "Results of SSSP Dijkstra Implementations"
 echo "========================"
 echo "sssp:"
 ./sssp ${file_name}
+echo ""
+echo "sssp_omp:"
+./sssp_omp ${file_name} ${threads}
+echo "========================"
 rm $file_name
