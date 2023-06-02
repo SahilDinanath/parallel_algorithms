1f54ca72c682161f9b456e05d766f03633995f66
Sahil Dinanath <sahildinanath@gmail.com>
2023-06-02 16:11:31 +0200

    changed run scripts to now use power of 2 instead of file

--- scan/run.sh
+++ scan/run.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 #options for running files
-file_name=input_2_3.txt
-threads=4
-processes=4
+power_of_two=4
+threads=2
+processes=2
 
 make clean
 make all 
 
-
+echo ${num_of_elements}
 
 echo "========================"
 echo "Results of Scan Implementations"
 echo "========================"
 echo "scan:"
 #the parameters that scan take are ./scan <file_name> 
-./scan ${file_name}
+./scan ${power_of_two}
 echo ""
 echo "scan_omp:"
 #the parameters that scan_omp take are ./scan_omp <file_name> <threads>
-./scan_omp ${file_name} ${threads}
+./scan_omp ${power_of_two} ${threads}
 echo ""
 echo "scan_mpi:"
-mpirun -np ${processes} ./scan_mpi ${file_name} ${processes}
+mpirun -np ${processes} ./scan_mpi ${power_of_two} ${processes}
 echo ""
 echo "========================"
