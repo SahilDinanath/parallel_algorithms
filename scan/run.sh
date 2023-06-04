96ab65b7076d11db2628b04087b31b8e1f87c5ee
Sahil Dinanath <sahildinanath@gmail.com>
2023-06-04 14:41:02 +0200

    fixed run file

--- scan/run.sh
+++ scan/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #options for running files
-power_of_two=18
-threads=2
+power_of_two=28
+threads=8
 processes=2
 
 make clean
@@ -14,13 +14,23 @@ echo "Results of Scan Implementations"
 echo "========================"
 echo "scan:"
 #the parameters that scan take are ./scan <file_name> 
-./scan ${power_of_two}
-echo ""
-echo "scan_omp:"
+# for i in {1..4}
+# do
+# ./scan ${power_of_two}
+# echo ""
+# done
+# echo "scan_omp:"
+ for i in {1..4}
+ do
 #the parameters that scan_omp take are ./scan_omp <file_name> <threads>
+
 ./scan_omp ${power_of_two} ${threads}
 echo ""
+done
 echo "scan_mpi:"
+for i in {1..4}
+do
 mpirun -np ${processes} ./scan_mpi ${power_of_two} ${processes}
 echo ""
+done
 echo "========================"
