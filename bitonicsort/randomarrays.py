c1b7d28fa6bdc8beb1668874bbf056609bb4e153
Darshan Singh <88923579+GyuoMi@users.noreply.github.com>
2023-06-05 13:56:13 +0200

    bitonic sequences

--- bitonicsort/randomarrays.py
+++ bitonicsort/randomarrays.py
@@ -1,9 +1,19 @@
 import random
 
-file = open("input_2_18.txt","w")
+file = open("input_2_20.txt", "w")
 
-for i in range(2**18):
-    line = str(random.randint(0,9))
+def generate_bitonic_sequence(n):
+    seq = [random.randint(0, 9) for _ in range(n)]
+    seq.sort()  # Sorting the sequence in ascending order
+    mid = n // 2
+    seq[mid:] = reversed(seq[mid:])  # Reversing the second half of the sequence
+    return seq
+
+n = 2 ** 20  # Size of the bitonic sequence
+bitonic_seq = generate_bitonic_sequence(n)
+
+for num in bitonic_seq:
+    line = str(num)
     file.write(line)
 
 file.close()
